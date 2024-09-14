1c30a5f035ca681b6d37326a3ac559199414cf32
Larissa Pedraja <larissapedraja@gmail.com>
2024-09-13 22:31:51 -0300

    versao inicial

---
+++ src/main/java/com/belval/pizzaria/TesteController.java
@@ -0,0 +1,17 @@
+package com.belval.pizzaria;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //controller responde aquisicoes
+
+public class TesteController {
+	
+	@GetMapping("/")
+	
+	public String noAr() {
+		return "no ar!!!";
+	}
+	
+
+}
